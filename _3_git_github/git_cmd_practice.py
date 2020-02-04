96579aa46c7c0bcb9bdc2f5a240286918507b432
rajan <rajyotiroshan@gmail.com>
2020-02-04 20:54:49 +0530

    merge_conflict_2nd_branch

--- _3_git_github/git_cmd_practice.py
+++ _3_git_github/git_cmd_practice.py
@@ -2,3 +2,8 @@ def print_branch():
     print("Working in b-p-2 branch")
 
 print_branch()
+
+def merge_confict_2nd_branch():
+    print("creating merge conflict, commiting in 2nd branch:b-p-2 ")
+
+merge_confict_2nd_branch()

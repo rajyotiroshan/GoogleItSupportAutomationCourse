10302098656a1442f189f7ed8f495dfc23e70576
rajyotiroshan <rajangreedge@gmail.com>
2020-02-06 22:03:00 +0530

    remote branch created through github web
    created remote branch on github web

--- _3_git_github/git_cmd_practice.py
+++ _3_git_github/git_cmd_practice.py
@@ -12,3 +12,6 @@ def merge_confict_2nd_branch():
     print("creating merge conflict, commiting in 2nd branch:b-p-2 ")
 
 merge_confict_2nd_branch()
+
+def created_remote_branch():
+    print("created remote branch through github web, and try to merge/update the local repo.")
